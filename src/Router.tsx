0ebe87bb35e03cf1e239047629131688a9d20872
sanghyeokahn <jooa7878@naver.com>
2022-06-29 00:31:34 +0900

    deploy test

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,24 +1,25 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { useAppSelector } from "./hooks/hooks";
 import { Home } from "./page/Home";
 import { Login } from "./page/Login";
 
 export const Router = () => {
   const isLogin = useAppSelector((state) => state.users.user.isLogin);
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        {isLogin ? (
-          <>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </>
-        ) : (
-          <>
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          </>
-        )}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
       </Routes>
     </BrowserRouter>
   );
