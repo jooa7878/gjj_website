99dd781228deabca5ce3342f710239f7d1ef15cb
ahnsanghyeok <jooa7878@naver.com>
2022-11-08 18:47:36 +0900

    [FE] style : 회원 => 관원

--- frontend/pages/list/index.tsx
+++ frontend/pages/list/index.tsx
@@ -6,7 +6,7 @@ import { logout } from '../../utils/auth';
 const ListPage: NextPage = () => {
   return (
     <Layout title='구리 유도관 & 와이어 주짓수'>
-      <GNB text='회원 리스트' onClick={logout} />
+      <GNB text='관원 리스트' onClick={logout} />
     </Layout>
   );
 };
