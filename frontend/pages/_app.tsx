b76c78b210665b0230111d104389ee27ed3d9fc1
ahnsanghyeok <jooa7878@naver.com>
2022-11-11 18:47:21 +0900

    [FE] style : React Query Devtools 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import GlobalStyles from '../styles/GlobalStyles';
 import { RecoilRoot } from 'recoil';
 import PageWrapper from '../components/wrapper/PageWrapper';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,6 +25,7 @@ const App = ({ Component, pageProps }: AppProps & any) => {
         <PageWrapper>
           <Component {...pageProps} />
         </PageWrapper>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
   );
